e195eca0f53b5ff539310efdfe50c0f984577ca9
Otong Akan <cutytongy@gmail.com>
2022-08-30 14:44:10 +0100

    Added Alerts on successful booking

--- public/js/index.js
+++ public/js/index.js
@@ -91,5 +91,5 @@ if (bookBtn)
     bookTour(tourId);
   });
   
-const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 15);
+  const alertMessage = document.querySelector('body').dataset.alert;
+  if (alertMessage) showAlert('success', alertMessage, 13);
